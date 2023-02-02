6df3da49f8b8d1a759c7212b28d382a9cc8165a8
renekrewinkel <rene@studiokrijst.com>
2023-02-02 08:32:46 +0100

    Update for use with atomic-bomb v4.2.x - Added TypeScript support - use --platform react-ts

--- react-ts/[NAME].test.tsx
+++ react-ts/[NAME].test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react';
 
 
 /// Preferably each story instead of actual component

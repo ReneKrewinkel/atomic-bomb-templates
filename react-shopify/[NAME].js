2fe92e693a57924ebbabc21f03dbfde998ee39f9
renekrewinkel <rene@studiokrijst.com>
2023-03-14 16:20:59 +0100

    added Shopify template

---
+++ react-shopify/[NAME].js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+/// TODO: Define props for [NAME]
+const [NAME] = ({ testID = "", type = ""}) => {
+
+    return(
+        <div data-testid={ testID }
+             data-object-type={ type ?? ""}
+             className={ `[NAME]` }>
+        </div>
+    )
+
+}
+
+/// TODO: add more utility classes if needed!
+/// TODO: Adjust and extend
+const exceptionClasses = [
+    "option-1",
+    "option-2"
+]
+
+[NAME].propTypes = {
+    testID: PropTypes.string,
+    type: PropTypes.oneOf(exceptionClasses)
+}
+
+export default [NAME]

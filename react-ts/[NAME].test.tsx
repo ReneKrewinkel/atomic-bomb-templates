b7708a1e009b6d4f8341f5fd57e2ada3c4adf420
renekrewinkel <rene@studiokrijst.com>
2023-05-15 14:21:41 +0200

    Amended TS configuration

--- react-ts/[NAME].test.tsx
+++ react-ts/[NAME].test.tsx
@@ -11,7 +11,7 @@ const testID = "[NAME]-" + Math.floor(Math.random()*90000) + 10000
 
 describe("[NAME]", () => {
 
-    it("Can render Default[NAME]", () => {
+    it("Can render [NAME]", () => {
         render(<[NAME] testID={ testID } />)
         let defaultCreated = screen.getByTestId(testID)
         expect(defaultCreated).not.toBeNull()

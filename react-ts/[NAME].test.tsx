e6988049ccefff7b4747c3b56d7beb691ca9e1b3
renekrewinkel <rene@studiokrijst.com>
2023-07-11 09:36:52 +0200

    Add react-native typescript (expo)

--- react-ts/[NAME].test.tsx
+++ react-ts/[NAME].test.tsx
@@ -1,4 +1,3 @@
-import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { [NAME]Interface  } from './[NAME].interface'
 import [NAME] from './[NAME]'

52e69eb1a988af94a26e5853b4d9f0278fdede41
renekrewinkel <rene@krewinkel.nl>
2023-08-24 13:58:17 +0200

    Add react-native typescript (expo) bugfix on stylesheet

--- react-native-ts/[NAME].style.ts
+++ react-native-ts/[NAME].style.ts
@@ -1,10 +1,11 @@
+import { StyleSheet } from 'react-native'
 /// TODO: add general style(s)
 /// import Style from './path/to/general/style
 
-const [NAME]Style = {
+const [NAME]Style = StyleSheet.create({
   View: {
     padding: 10
   }
-}
+})
 
 export default [NAME]Style

fc3e04a951708885473e820a05af4b06d65aed38
renekrewinkel <rene@krewinkel.nl>
2023-09-22 10:09:49 +0200

    added description to tw-ts

--- react-ts-tw/[NAME].test.tsx
+++ react-ts-tw/[NAME].test.tsx
@@ -5,11 +5,11 @@ import [NAME] from './[NAME]'
 import { [NAME]Mock } from './[NAME].mock'
 
 /// TODO: fix test based upon interface
-const testID = "[NAME]-" + Math.floor(Math.random()*90000) + 10000
+const testID = '[NAME]-' + Math.floor(Math.random()*90000) + 10000
 
-describe("[NAME]", () => {
+describe('[NAME]', () => {
 
-    it("Can render [NAME]", () => {
+    it('Can render [NAME]', () => {
         render(<[NAME] testID={ testID } { ...[NAME]Mock } />)
         let defaultCreated = screen.getByTestId(testID)
         expect(defaultCreated).not.toBeNull()

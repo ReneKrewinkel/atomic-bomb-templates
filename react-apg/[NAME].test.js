984ec8131ed8a2eeadd90f2b78b0901bee561672
renekrewinkel <rene@krewinkel.nl>
2024-03-14 12:37:59 +0100

    APG Specifiek

--- react-apg/[NAME].test.js
+++ react-apg/[NAME].test.js
@@ -4,14 +4,14 @@ import [NAME]Mock from './[NAME].mock'
 
 /// Preferably each story instead of actual object
 /// TODO: fix test based upon story entries
-import { Default[NAME] } from './[NAME].stories'
+import NAME]  from './[NAME]'
 
 const testID = "[NAME]-" + Math.floor(Math.random()*90000) + 10000
 
 describe("[NAME]", () => {
 
     it("Can render [NAME]", () => {
-        render(<Default[NAME] testID={ testID } { ...[NAME]Mock } />)
+        render(<[NAME] testID={ testID } { ...[NAME]Mock } />)
         let defaultCreated = screen.getByTestId(testID)
         expect(defaultCreated).not.toBeNull()
     })

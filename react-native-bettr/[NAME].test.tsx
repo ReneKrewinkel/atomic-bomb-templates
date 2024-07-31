9ee2d3eae3dcd7753498e9601dddca7d9cbf2fec
renekrewinkel <rene@krewinkel.nl>
2024-07-31 14:57:42 +0200

    Specific Template for Bettr

--- react-native-bettr/[NAME].test.tsx
+++ react-native-bettr/[NAME].test.tsx
@@ -1,13 +1,27 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react-native'
 
 import { [NAME]Interface  } from './[NAME].interface'
 import [NAME] from './[NAME]'
 import { [NAME]Mock } from './[NAME].mock'
 
+const mockedNavigation = jest.fn();
+jest.mock('@react-navigation/native', () => {
+    return {
+        useNavigation: () => ({
+            navigate: mockedNavigation,
+        }),
+    }
+})
+
 /// TODO: fix test based upon interface
 const testID = "[NAME]-" + Math.floor(Math.random()*90000) + 10000
 
 describe("[NAME]", () => {
+
+    beforeEach(() => {
+        mockedNavigation.mockClear();
+    })
 
     it("Can render [NAME]", () => {
         render(<[NAME] testID={ testID } { ...[NAME]Mock } />)

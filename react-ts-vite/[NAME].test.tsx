eb2c5ad75976cca0ebd7a03fb9e87731e6185b4d
René Krewinkel <rene@studiokrijst.com>
2025-04-01 14:04:25 +0200

    Added react typescript vite templates

---
+++ react-ts-vite/[NAME].test.tsx
@@ -0,0 +1,18 @@
+import {describe, it, expect} from 'vitest';
+import { render, screen } from '@testing-library/react'
+import { [NAME]Interface  } from './[NAME].interface'
+import [NAME] from './[NAME]'
+import { [NAME]Mock } from './[NAME].mock'
+
+/// TODO: fix test based upon interface
+const testID = "[NAME]-" + Math.floor(Math.random()*90000) + 10000
+
+describe("[NAME]", () => {
+
+    it("Can render [NAME]", () => {
+        render(<[NAME] testID={ testID } { ...[NAME]Mock } />)
+        let defaultCreated = screen.getByTestId(testID)
+        expect(defaultCreated).not.toBeNull()
+    })
+
+})

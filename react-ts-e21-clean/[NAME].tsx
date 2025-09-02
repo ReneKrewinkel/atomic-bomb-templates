dd39c510e591826c21987199ee6855f6e225f7c1
René Krewinkel <rene@studiokrijst.com>
2025-09-02 08:48:21 +0200

    Update typescript test and storyfiles

---
+++ react-ts-e21-clean/[NAME].tsx
@@ -0,0 +1,16 @@
+// import { useState, useEffect, useContext } from 'react'
+import { [NAME]Interface } from './[NAME].interface'
+
+/// TODO: update interface/arguments for [NAME]
+const [NAME] = ({testID, style, type, size, props }:[NAME]Interface) => {
+
+    return(
+        <div data-testid={ testID }
+             data-object-type={ type ?? ""}
+             className={ `[NAME]` }>
+        </div>
+    )
+
+}
+
+export default [NAME]

2c9af41179f3ea8a3f4d657a572f66affdf7a0f1
René Krewinkel <15904823+ReneKrewinkel@users.noreply.github.com>
2025-12-04 19:34:09 +0100

    Update test TODO comment

--- react-native-ts/[NAME].test.tsx
+++ react-native-ts/[NAME].test.tsx
@@ -4,7 +4,7 @@ import { [NAME]Interface  } from './[NAME].interface'
 import [NAME] from './[NAME]'
 import { [NAME]Mock } from './[NAME].mock'
 
-/// TODO: fix test based upon interface
+/// TODO: fix [NAME] test based upon interface
 const testID = "[NAME]-" + Math.floor(Math.random()*90000) + 10000
 
 describe("[NAME]", () => {
